c3e1893ddafe83eee885c8fc076f2ec7c053d140
RiddhiGandhi1 <91381400+RiddhiGandhi1@users.noreply.github.com>
2021-12-02 22:19:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const mongoose = require("mongoose");
 
